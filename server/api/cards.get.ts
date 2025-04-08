e795aa09cf7fcbd9197694ba74d30d8c2b946b00
Josh Evensen <joshevensen@outlook.com>
2025-04-07 20:04:09 -0500

    more changes

--- server/api/cards.get.ts
+++ server/api/cards.get.ts
@@ -1,7 +1,7 @@
-import {PrismaClient} from "@prisma/client";
-
 export default defineEventHandler(async (event) => {
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
+  //
+  // return prisma.card.findMany({include: {Collection: true}});
 
-  return prisma.card.findMany({include: {Collection: true}});
+  return []
 })
